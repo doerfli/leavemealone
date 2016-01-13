0c70f5cae30a9d0afbe1d106ddc3b1d15349f601
doerfli <github@doerf.li>
2016-01-13 22:56:57 +0100

    request permission for READ_PHONE_STATE when enabling callblocker in api >=23 (#28)

--- app/src/main/java/li/doerf/leavemealone/activities/SettingsActivity.java
+++ app/src/main/java/li/doerf/leavemealone/activities/SettingsActivity.java
@@ -1,8 +1,14 @@
 package li.doerf.leavemealone.activities;
 
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import li.doerf.leavemealone.LeaveMeAloneApplication;
+import li.doerf.leavemealone.R;
 import li.doerf.leavemealone.ui.fragments.SettingsFragment;
 
 /**
@@ -10,14 +16,38 @@ import li.doerf.leavemealone.ui.fragments.SettingsFragment;
  */
 public class SettingsActivity extends AppCompatActivity {
     private final String LOGTAG = getClass().getSimpleName();
+    private SettingsFragment mySettingsFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mySettingsFragment = new SettingsFragment();
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(android.R.id.content, mySettingsFragment)
                 .commit();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case LeaveMeAloneApplication.PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(LOGTAG, "permission READ_PHONE_STATE granted by user");
+                    mySettingsFragment.handleSettingChanged(getString(R.string.pref_key_master_switch));
+                } else {
+                    Log.i(LOGTAG, "permission READ_PHONE_STATE denied by user");
+                    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putBoolean(getString(R.string.pref_key_master_switch), false);
+                    editor.commit();
+                    mySettingsFragment.refresh();
+                }
+                break;
+            }
+        }
     }
 }
